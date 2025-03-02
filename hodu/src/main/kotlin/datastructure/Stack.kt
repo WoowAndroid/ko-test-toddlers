db90dda59cd43e767d4cdf7acca71a6a3c04f139
Junyoung-WON <wjyo60@gmail.com>
2025-03-02 18:00:50 +0900

    refactor: _stack 프로퍼티 이름 수정
    - _stack -> stack

--- hodu/src/main/kotlin/datastructure/Stack.kt
+++ hodu/src/main/kotlin/datastructure/Stack.kt
@@ -17,20 +17,20 @@ class StackUsingTop<T : Any?>(private val maxSize: Int) : Stack<T> {
     override var top: Int = -1
         private set
     override val size: Int
-        get() = _stack.size
-    private val _stack: Array<T?> = Array<Any?>(maxSize) {null} as Array<T?>
+        get() = stack.size
+    private val stack: Array<T?> = Array<Any?>(maxSize) {null} as Array<T?>
 
     init {
         require(maxSize > 0) { "스택의 크기는 0보다 커야합니다." }
     }
 
     override fun push(item: T) {
-        if (top < maxSize - 1) _stack[++top] = item
+        if (top < maxSize - 1) stack[++top] = item
         else println("더 이상 넣을 수 없습니다. Max Size: $maxSize")
     }
 
     override fun pop(): T? {
-        return if (!isEmpty()) _stack[top--]
+        return if (!isEmpty()) stack[top--]
         else {
             println("스택이 비어있습니다. null을 반환합니다.")
             null
@@ -38,7 +38,7 @@ class StackUsingTop<T : Any?>(private val maxSize: Int) : Stack<T> {
     }
 
     override fun peek(): T? {
-        return if (!isEmpty()) _stack[top]
+        return if (!isEmpty()) stack[top]
         else {
             println("스택이 비어있습니다. null을 반환합니다.")
             null
