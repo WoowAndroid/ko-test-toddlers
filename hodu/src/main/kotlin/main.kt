dfd4d15a884e36adc1dcef6361e6be300f532a75
Junyoung-WON <wjyo60@gmail.com>
2025-03-03 16:14:36 +0900

    feat: 최대 힙, 최소 힙 구현

--- hodu/src/main/kotlin/main.kt
+++ hodu/src/main/kotlin/main.kt
@@ -1,12 +1,11 @@
-import datastructure.Queue
-import datastructure.QueueWithIndex
-import datastructure.Stack
-import datastructure.StackUsingTop
+import datastructure.*
 
 fun main() {
     println("hello walnut")
 //    testStack()
-    testQueue()
+//    testQueue()
+    testMinHeap()
+    testMaxHeap()
 }
 
 private fun testStack() {
@@ -52,4 +51,82 @@ private fun testQueue() {
     println("dequeue: ${queue.dequeue()}")
     println("size: ${queue.size}, front: ${queue.front}, rear: ${queue.rear}")
 
+}
+
+private fun testMinHeap() {
+    val minHeap: MinHeap = MinHeap(10)
+    println("--- [최소 힙 테스트] ---")
+    println("delete: ${minHeap.delete()}")
+    println(minHeap.isEmpty())
+
+    minHeap.insert(3)
+    minHeap.insert(5)
+    minHeap.insert(10)
+    minHeap.insert(2)
+    minHeap.insert(9)
+    println("마지막 노드 번호: ${minHeap.endNodeIndex}")
+    for (i in 1..minHeap.size) {
+        print("$i[${minHeap.heap[i]}]  ")
+    }
+    println()
+
+    minHeap.insert(15)
+    minHeap.insert(2)
+    minHeap.insert(-3)
+    minHeap.insert(-6)
+    minHeap.insert(0)
+    minHeap.insert(100)
+    println("마지막 노드 번호: ${minHeap.endNodeIndex}")
+    for (i in 1..minHeap.size) {
+        print("$i[${minHeap.heap[i]}]  ")
+    }
+    println()
+
+    println("delete: ${minHeap.delete()}")
+    println("delete: ${minHeap.delete()}")
+    println("delete: ${minHeap.delete()}")
+    println("마지막 노드 번호: ${minHeap.endNodeIndex}")
+    for (i in 1..minHeap.size) {
+        print("$i[${minHeap.heap[i]}]  ")
+    }
+    println()
+}
+
+private fun testMaxHeap() {
+    val maxHeap: MaxHeap = MaxHeap(10)
+    println("--- [최대 힙 테스트] ---")
+    println("delete: ${maxHeap.delete()}")
+    println(maxHeap.isEmpty())
+
+    maxHeap.insert(3)
+    maxHeap.insert(5)
+    maxHeap.insert(10)
+    maxHeap.insert(2)
+    maxHeap.insert(9)
+    println("마지막 노드 번호: ${maxHeap.endNodeIndex}")
+    for (i in 1..maxHeap.size) {
+        print("$i[${maxHeap.heap[i]}]  ")
+    }
+    println()
+
+    maxHeap.insert(15)
+    maxHeap.insert(2)
+    maxHeap.insert(-3)
+    maxHeap.insert(-6)
+    maxHeap.insert(0)
+    maxHeap.insert(100)
+    println("마지막 노드 번호: ${maxHeap.endNodeIndex}")
+    for (i in 1..maxHeap.size) {
+        print("$i[${maxHeap.heap[i]}]  ")
+    }
+    println()
+
+    println("delete: ${maxHeap.delete()}")
+    println("delete: ${maxHeap.delete()}")
+    println("delete: ${maxHeap.delete()}")
+    println("마지막 노드 번호: ${maxHeap.endNodeIndex}")
+    for (i in 1..maxHeap.size) {
+        print("$i[${maxHeap.heap[i]}]  ")
+    }
+    println()
 }
