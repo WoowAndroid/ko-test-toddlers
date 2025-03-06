49ec8b30c6da6ec747ebced354c6888d91be9fbc
somin <salth0601@gmail.com>
2025-03-06 12:28:55 +0900

    bingT: 오타수정

--- bingT/src/main/kotlin/구현/21918. 전구/전구.kt
+++ bingT/src/main/kotlin/구현/21918. 전구/전구.kt
@@ -4,26 +4,25 @@ import java.io.InputStreamReader
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val input = br.readLine().split(" ").map { it.toInt() }
-    val blobNumbers: Int = input[0]
     val commandNumbers: Int = input[1]
-    val blobs: IntArray = br.readLine().split(" ").map { it.toInt() }.toIntArray()
+    val bulbs: IntArray = br.readLine().split(" ").map { it.toInt() }.toIntArray()
     repeat(commandNumbers) {
         val command = br.readLine().split(" ").map { it.toInt() }
         when (command[0]) {
             1 -> {
-                change(blobs, command[1], command[2])
+                change(bulbs, command[1], command[2])
             }
 
             2 -> {
-                reverse(blobs, command[1], command[2])
+                reverse(bulbs, command[1], command[2])
             }
 
             3 -> {
-                turnOff(blobs, command[1], command[2])
+                turnOff(bulbs, command[1], command[2])
             }
 
             4 -> {
-                turnOn(blobs, command[1], command[2])
+                turnOn(bulbs, command[1], command[2])
             }
 
             else -> {
@@ -31,27 +30,27 @@ fun main() {
             }
         }
     }
-    println(blobs.joinToString(" "))
+    println(bulbs.joinToString(" "))
 }
 
-fun change(blobs: IntArray, i: Int, x: Int) {
-    blobs[i-1] = x
+fun change(bulbs: IntArray, i: Int, x: Int) {
+    bulbs[i-1] = x
 }
 
-fun turnOff(blobs: IntArray, l: Int, r: Int) {
+fun turnOff(bulbs: IntArray, l: Int, r: Int) {
     for (i in l-1..r-1) {
-        blobs[i] = 0
+        bulbs[i] = 0
     }
 }
 
-fun turnOn(blobs: IntArray, l: Int, r: Int) {
+fun turnOn(bulbs: IntArray, l: Int, r: Int) {
     for (i in l-1..r-1) {
-        blobs[i] = 1
+        bulbs[i] = 1
     }
 }
 
-fun reverse(blobs: IntArray, l: Int, r: Int) {
+fun reverse(bulbs: IntArray, l: Int, r: Int) {
     for (i in l-1..r-1) {
-        blobs[i] = (blobs[i] + 1) % 2
+        bulbs[i] = (bulbs[i] + 1) % 2
     }
 }
